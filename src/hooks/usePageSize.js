07a15792b47e0ed4fd88e2eb92ee6c5781879bf0
ezzzni <unin5412@gmail.com>
2026-05-06 15:06:06 +0900

    refactor: PAGE_SIZE를 구할 때 기존 하드코딩 방식에서 반응형 설정값을 객체로 분리하는 방식으로 변경

--- src/hooks/usePageSize.js
+++ src/hooks/usePageSize.js
@@ -1,20 +1,19 @@
 import { useMediaQuery } from "react-responsive";
 
+const PAGE_SIZE = {
+  best: { desktop: 4, tablet: 2, mobile: 1 },
+  forSale: { desktop: 10, tablet: 6, mobile: 4 },
+};
+
 function usePageSize(type = "best") {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
   const isTablet = useMediaQuery({ minWidth: 768 });
+  const sizes = PAGE_SIZE[type];
 
-  if (type === "best") {
-    if (isDesktop) return 4;
-    if (isTablet) return 2;
-    return 1;
-  }
+  if (isDesktop) return sizes.desktop;
+  if (isTablet) return sizes.tablet;
 
-  if (type === "forSale") {
-    if (isDesktop) return 10;
-    if (isTablet) return 6;
-    return 4;
-  }
+  return sizes.mobile;
 }
 
 export default usePageSize;
