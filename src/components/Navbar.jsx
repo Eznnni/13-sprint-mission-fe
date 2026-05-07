17b552be3839cd35ddcfa1018c890672d7fa2610
ezzzni <unin5412@gmail.com>
2026-05-07 11:49:11 +0900

    feat: Navbar component 구현

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+import { NavLink } from "react-router";
+import "../styles/navbar.css";
+import { useMediaQuery } from "react-responsive";
+import logoMobile from "/logo_mobile.png";
+import logoPC from "/logo.svg";
+
+function Navbar() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  return (
+    <nav className="header-container">
+      <div className="header-left-section">
+        <Link to="/">
+          <img src={isMobile ? logoMobile : logoPC} alt="판다마켓 홈" />
+        </Link>
+        <div className="header-left-nav">
+          <Link to="#" className="header-menu-item">
+            <span className="header-menu-item-tag">자유게시판</span>
+          </Link>
+          <NavLink to="/items" className="header-menu-item">
+            <span className="header-menu-item-tag">중고마켓</span>
+          </NavLink>
+        </div>
+      </div>
+      <Link to="/login" id="login-button" className="button">
+        로그인
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
