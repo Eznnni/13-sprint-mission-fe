ba8ae5c4f2bac755d0d8d3b18bdc405acf584858
ezzzni <unin5412@gmail.com>
2026-05-11 11:57:40 +0900

    feat: 마이그레이션으로 인한 class -> className 으로 변경 및 랜딩 페이지 반응형 대응

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,11 @@ import bottomImg from "../assets/img/landing/Img_home_bottom.png";
 function Hero() {
   return (
     <>
-      <section class="main-blue">
-        <div class="main-container">
-          <div class="left-section">
-            <h1 class="mention">일상의 모든 물건을 거래해 보세요</h1>
-            <Link class="items-bttn" to="/items">
+      <section className="main-blue">
+        <div className="main-container">
+          <div className="left-section">
+            <h1 className="mention">일상의 모든 물건을 거래해 보세요</h1>
+            <Link className="items-bttn" to="/items">
               구경하러 가기
             </Link>
           </div>
@@ -21,25 +21,25 @@ function Hero() {
         </div>
       </section>
 
-      <section class="main-white">
-        <div class="contents-wrapper">
+      <section className="main-white">
+        <div className="contents-wrapper">
           <img src={homeImg1} alt="인기 상품 이미지" />
-          <div class="right-section">
-            <div class="highlight-keyword">Hot item</div>
-            <h2 class="mention">인기 상품을 확인해 보세요</h2>
-            <p class="description">
+          <div className="right-section">
+            <div className="highlight-keyword">Hot item</div>
+            <h2 className="mention">인기 상품을 확인해 보세요</h2>
+            <p className="description">
               가장 HOT한 중고거래 물품을 판다 마켓에서 확인해 보세요
             </p>
           </div>
         </div>
       </section>
 
-      <section class="main-white">
-        <div class="contents-wrapper ">
-          <div class="search-left-section">
-            <div class="highlight-keyword">Search</div>
-            <h2 class="mention">구매를 원하는 상품을 검색하세요</h2>
-            <p class="description">
+      <section className="main-white">
+        <div className="contents-wrapper ">
+          <div className="search-left-section">
+            <div className="highlight-keyword">Search</div>
+            <h2 className="mention">구매를 원하는 상품을 검색하세요</h2>
+            <p className="description">
               구매하고 싶은 물품은 검색해서 쉽게 찾아보세요
             </p>
           </div>
@@ -47,22 +47,22 @@ function Hero() {
         </div>
       </section>
 
-      <section class="main-white">
-        <div class="contents-wrapper">
+      <section className="main-white">
+        <div className="contents-wrapper">
           <img src={homeImg3} alt="등록 이미지" />
-          <div class="right-section">
-            <div class="highlight-keyword">Register</div>
-            <h2 class="mention">판매를 원하는 상품을 등록하세요</h2>
-            <p class="description">
+          <div className="right-section">
+            <div className="highlight-keyword">Register</div>
+            <h2 className="mention">판매를 원하는 상품을 등록하세요</h2>
+            <p className="description">
               어떤 물건이든 판매하고 싶은 상품을 쉽게 등록하세요
             </p>
           </div>
         </div>
       </section>
 
-      <section class="main-blue--bottom">
-        <div class="main-container-bottom">
-          <h2 class="mention">믿을 수 있는 판다마켓 중고 거래</h2>
+      <section className="main-blue--bottom">
+        <div className="main-container-bottom">
+          <h2 className="mention">믿을 수 있는 판다마켓 중고 거래</h2>
           <img src={bottomImg} alt="판다 대화 이미지" />
         </div>
       </section>
