ba8af051db2290e8a0255b68dad3ee9f36c87f35
ezzzni <unin5412@gmail.com>
2026-06-17 14:38:32 +0900

    feat: 헤더 컴포넌트 구현
    - global.css에 스타일 변수 설정 및 nav-link와 btn 컴포넌트 스타일 설정
    - Pretendard 폰트 적용
    - Header 컴포넌트 구현
    - Header 컴포넌트의 NAV_LINKS를 상수화

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,20 @@
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
+import "./globals.css";
+import localFont from "next/font/local";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+
+const pretendard = localFont({
+  src: "../fonts/Pretendard-Regular.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko">
-      <body>
+    <html lang="ko" className={pretendard.className}>
+      <body className="font-sans">
         <Header />
-        <main>{children}</main>
+        <main className="pt-17.5">{children}</main>
         <Footer />
       </body>
     </html>
---
+++ src/constants/navigation.js
@@ -0,0 +1,4 @@
+export const NAV_LINKS = [
+  { id: 1, href: "/community", label: "자유게시판" },
+  { id: 2, href: "/market", label: "중고마켓" },
+];
