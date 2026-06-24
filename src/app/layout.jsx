675660972614c7613cb2ceb196fad813c0e03d51
ezzzni <unin5412@gmail.com>
2026-06-24 21:03:08 +0900

    feat: 로그인/회원가입 페이지의 header와 footer가 제거 layout 수정
    - signin과 signup 폴더를 (auth) 하위로 이동
    - 제외한 모든 폴더를 (main) 으로 이동
    - RootLayout에 header footer 제거
    - MainLayout에 header footer 유지

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import localFont from "next/font/local";
-import Header from "../components/layout/Header";
-import Footer from "../components/layout/Footer";
 
 const pretendard = localFont({
   src: "../fonts/Pretendard-Regular.woff2",
@@ -17,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" className={pretendard.className}>
-      <body className="flex min-h-screen flex-col font-sans">
-        <Header />
-        <main className="grow pt-17.5">{children}</main>
-        <Footer />
-      </body>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
