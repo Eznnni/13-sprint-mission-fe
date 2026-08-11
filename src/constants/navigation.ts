f604aef5d1d29a18fead13583bb2a5ec6b4f67b8
ezzzni <unin5412@gmail.com>
2026-08-11 16:39:30 +0900

    chore: endpoint, navigation 상수 등록

---
+++ src/constants/navigation.ts
@@ -0,0 +1,41 @@
+import facebook from "@/assets/icons/social/facebook-logo.svg";
+import instagram from "@/assets/icons/social/instagram-logo.svg";
+import twitter from "@/assets/icons/social/twitter-logo.svg";
+import youtube from "@/assets/icons/social/youtube-logo.svg";
+
+export const ROUTES = {
+  HOME: "/",
+  COMMUNITY: {
+    BASE: "/community",
+    REGISTER: "/community/register",
+    DETAIL: (id: string) => `/community/${id}`,
+    EDIT: (id: string) => `/community/${id}/edit`,
+  },
+  ITEM: {
+    BASE: "/items",
+    DETAIL: (id: string) => `/items/${id}`,
+    EDIT: (id: string) => `/items/${id}/edit`,
+  },
+};
+
+export const NAV_LINKS = [
+  { id: 1, href: "/community", label: "자유게시판" },
+  { id: 2, href: "/items", label: "중고마켓" },
+];
+
+export const SOCIAL_LINKS = [
+  {
+    id: 1,
+    href: "https://www.facebook.com/",
+    src: facebook,
+    alt: "페이스북",
+  },
+  { id: 2, href: "https://twitter.com/", src: twitter, alt: "트위터" },
+  { id: 3, href: "https://www.youtube.com/", src: youtube, alt: "유튜브" },
+  {
+    id: 4,
+    href: "https://www.instagram.com/",
+    src: instagram,
+    alt: "인스타그램",
+  },
+];
