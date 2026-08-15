bc80802f420dad2022c8630019bc43aa39d4cc6c
ezzzni <unin5412@gmail.com>
2026-08-15 19:20:19 +0900

    feat: register/edit FormData type 생성

--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -5,4 +5,8 @@ export const registerPostSchema = z.object({
   content: z.string().min(5, "게시글 내용은 5자 이상이어야 합니다"),
 });
 
+export type RegisterPostFormData = z.infer<typeof registerPostSchema>;
+
 export const editPostSchema = registerPostSchema.partial();
+
+export type EditPostFormData = z.infer<typeof editPostSchema>;
